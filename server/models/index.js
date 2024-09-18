f40ae64d50da39536ff2b141a04fd43bb550440c
Olivier Ndicunguye <nolivanoly@gmail.com>
2024-09-17 19:27:57 -0700

    added roleData and also added some code for the model specifically in index.js of the models

--- server/models/index.js
+++ server/models/index.js
@@ -3,7 +3,8 @@ const Appointment = require('./Appointment');
 const Practitioner = require('./Practitioner');
 const Service = require('./Service');
 const Review = require('./Review');
+const Role = require('./Role');
 
 
 
-module.exports = { User, Appointment, Practitioner, Service, Review };
+module.exports = { User, Appointment, Practitioner, Service, Review, Role };
