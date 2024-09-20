5338f5d9910502d8280d6b4b5f15aa2470c271c8
MarcM <marina.ved80gmail.com>
2024-09-20 12:22:54 -0600

    navTabs style

--- client/src/components/NavTabs.jsx
+++ client/src/components/NavTabs.jsx
@@ -1,31 +1,39 @@
 import { color } from 'framer-motion';
 import { Link } from 'react-router-dom';
-import './style/';
-
-// const styles = {
-//   navContainer: {
-//     // display: flex,
-//     margin: 0,
-//     background: '#2d3e50',
-//     // color: white,
-//   },
-
-// };
+import './style/navtabs.css';
 
 export default function NavTabs({ links }) {
   return (
     // <nav className="navbar navbar-expand-lg bg-secondary" style={styles.navContainer}>
-    <nav className="navbar navbar-expand-lg bg-secondary navContainer" >
+    <nav className="navbar navbar-expand-lg bg-secondary" >
       <div className="container-fluid">
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <div className="navbar-nav me-auto mb-2 mb-lg-0">
-            <div><Link to="/">Home</Link></div>
-            <div><Link to="/about">About Us</Link></div>
-            <div><Link to="/services">Services</Link></div>
-            <div><Link to="/contact">Contact</Link></div>
-            <div><Link to="/signup">Sign Up</Link></div>
-            <div><Link to="/login">Login</Link></div>
-            <div><Link to="/logout">Logout</Link></div>
+          <div className="navbar-nav me-auto mb-2 mb-lg-0 navContainer">
+            <div className="rightHand navLink">
+                <div className="navLink">
+                  <Link to="/">Home</Link>
+                </div>
+                <div className="navLink">
+                  <Link to="/about">About Us</Link>
+                </div>
+                <div className="navLink">
+                  <Link to="/services">Services</Link>
+                </div>
+                <div className="navLink">
+                  <Link to="/contact">Contact</Link>
+                </div>
+            </div>
+            <div className="leftHand navLink">
+                <div className="navLink">
+                  <Link to="/signup">Sign Up</Link>
+                </div>
+                <div className="navLink">
+                  <Link to="/login">Login</Link>
+                </div>
+                <div className="navLink">
+                  <Link to="/logout">Logout</Link>
+                </div>
+            </div>
           </div>
         </div>
       </div>
