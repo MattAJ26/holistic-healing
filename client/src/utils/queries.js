7132bd9175dbe95a14d7db504c6608e8057696c9
Olivier Ndicunguye <nolivanoly@gmail.com>
2024-09-19 21:04:29 -0700

    Seed fixed

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,159 +1,30 @@
 import { gql } from '@apollo/client';
 
+export const QUERY_TECH = gql`
+  query tech {
+    tech {
+      _id
+      name
+    }
+  }
+`;
+
+
 export const QUERY_USERS = gql`
-  query getAllUsers {
-    users {
-      _id
-      username
-      email
-      roles {
-        _id
-        description
-        name
-      }
-    }
-  }
-`;
-
-export const QUERY_SINGLE_USER = gql`
-  query getSingleUser($userId: ID!) {
-    user(id: $userId) {
-      _id
-      email
-      roles {
-        _id
-        description
-        name
-      }
-    }
-  }
-`;
-
-export const QUERY_SERVICES = gql`
-  query getAllServices {
-    services {
+  query tech {
+    tech {
       _id
       name
-      duration
-      description
-      price
-      practitioners {
-        name
-        ratings {
-          _id
-          comment
-          rating
-          user {
-            username
-            _id
-          }
-        }
-      }
     }
   }
 `;
 
-export const QUERY_SINGLE_ROLES = gql`
-  query getSingleRole($roleId: ID!) {
-    role(id: $roleId) {
-      name
-      description
-      _id
-    }
-  }
-`;
 
-export const QUERY_REVIEWS = gql`
-  query getAllReviews {
-    reviews {
-      user {
-        username
-        _id
-      }
-      practitioner {
-        _id
-        name
-        specialties
-      }
-      service {
-        _id
-        name
-        duration
-        price
-      }
-      rating
-      comment
-    }
-  }
-`;
 
-export const QUERY_PRACTITIONERS = gql`
-  query getAllPractitioners {
-    practitioners {
-      _id
-      availableDates
-      bio
-      certifications
-      name
-      ratings {
-        rating
-        service {
-          name
-          description
-          _id
-        }
-      }
-    }
-  }
-`;
 
-export const QUERY_SINGLE_PRACTITIONER = gql`
-  query Practitioner($practitionerId: ID!) {
-    practitioner(id: $practitionerId) {
-      _id
-      name
-      bio
-      availableDates
-      certifications
-      specialties
-      ratings {
-        rating
-        comment
-        _id
-        service {
-          name
-          _id
-          description
-        }
-      }
-    }
-  }
-`;
-
-export const QUERY_APPOINTMENTS = gql`
-  query Query {
-    appointments {
-      _id
-      appointmentDate
-      notes
-      user {
-        username
-        email
-        _id
-      }
-      practitioner {
-        name
-        _id
-        availableDates
-      }
-      status
-    }
-  }
-`;
-
-export const QUERY_SINGLE_APPOINTMENT = gql`
-  query Query($appointmentId: ID!) {
-    appointment(id: $appointmentId) {
+export const QUERY_MATCHUPS = gql`
+  query matchups($_id: String) {
+    matchups(_id: $_id) {
       _id
       appointmentDate
       notes
