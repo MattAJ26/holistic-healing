9c4697eed1ceb2d11142bdb1a0400711ad204b21
MarcM <marina.ved80gmail.com>
2024-09-20 11:44:06 -0600

    navTab style1

--- client/src/components/NavTabs.jsx
+++ client/src/components/NavTabs.jsx
@@ -1,19 +1,32 @@
+import { color } from 'framer-motion';
 import { Link } from 'react-router-dom';
+import './style/';
+
+// const styles = {
+//   navContainer: {
+//     // display: flex,
+//     margin: 0,
+//     background: '#2d3e50',
+//     // color: white,
+//   },
+
+// };
 
 export default function NavTabs({ links }) {
   return (
-    <nav className="navbar navbar-expand-lg bg-secondary">
+    // <nav className="navbar navbar-expand-lg bg-secondary" style={styles.navContainer}>
+    <nav className="navbar navbar-expand-lg bg-secondary navContainer" >
       <div className="container-fluid">
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About Us</Link></li>
-            <li><Link to="/services">Services</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/logout">Logout</Link></li>
-          </ul>
+          <div className="navbar-nav me-auto mb-2 mb-lg-0">
+            <div><Link to="/">Home</Link></div>
+            <div><Link to="/about">About Us</Link></div>
+            <div><Link to="/services">Services</Link></div>
+            <div><Link to="/contact">Contact</Link></div>
+            <div><Link to="/signup">Sign Up</Link></div>
+            <div><Link to="/login">Login</Link></div>
+            <div><Link to="/logout">Logout</Link></div>
+          </div>
         </div>
       </div>
     </nav>
