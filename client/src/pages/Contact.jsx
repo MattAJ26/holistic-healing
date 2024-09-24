01655aec7226f3d4a4133164918f0b02bf6cb479
Olivier Ndicunguye <nolivanoly@gmail.com>
2024-09-23 23:41:57 -0700

    added contact form and the make sure the email is being sent

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,73 +1,142 @@
-import '../style/Contact.css';
-import { Link } from 'react-router-dom';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import { 
+  useDisclosure, 
+  Box, 
+  FormControl, 
+  FormLabel, 
+  Input, 
+  Textarea, 
+  Button, 
+  VStack, 
+  Modal, 
+  ModalOverlay, 
+  ModalContent, 
+  ModalHeader, 
+  ModalFooter, 
+  ModalBody, 
+  ModalCloseButton, 
+  Flex, 
+  Heading, 
+  Text 
+} from '@chakra-ui/react';
 
-export default function Contact() {
-    return (
-      // <div className="card bg-white card-rounded w-75 view">
-      <div className="card mainContainer card-rounded w-75 view">
-        <div>
+const Contact = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const form = useRef();
 
-          <div className="card-header bg-dark text-center">
-            <h1>Contact Page</h1>
-          </div>
-      
-          <div className="card-body m-5">
-            <h2 className="text-center pgTitle">Contact information</h2>
-            <div className="infoContact">
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-              <div className="phoneContact">
+    emailjs
+      .sendForm('service_6thn1ir', 'template_2de1svh', form.current, 'baT40mDhsq67bU9vc')
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          form.current.reset(); 
+          onOpen();
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        }
+      );
+  };
 
-                <div  className="titleContact" >
-                    <p className="center"><h3>Call Us:</h3></p>
-                </div>
+  return (
+    <>
+      <Flex
+        direction={{ base: 'column', lg: 'row' }}
+        justify="space-between"
+        align="center"
+        p={8}
+        mt={10}
+        bg="gray.50"
+      >
+       
+        <Box
+          flex="1"
+          p={8}
+          bg="white"
+          borderWidth={1}
+          borderRadius="lg"
+          boxShadow="xl"
+          // Set width to 80% on large screens
+          width={{ base: "100%", lg: "80%" }}
+        >
+          <Heading as="h2" size="xl" mb={6}>
+            Contact Us
+          </Heading>
 
-                <div className="infoC">
+          <form onSubmit={sendEmail} ref={form}>
+            <VStack spacing={4} align="flex-start">
+              <FormControl isRequired>
+                <FormLabel>Your Name</FormLabel>
+                <Input name="name" placeholder="Enter your name" type="text" />
+              </FormControl>
 
-                    <div className="infoContact">
-                      <div className="unicode-icon">&#10137;</div>
-                      <p className="left">  Monday - Fraiday</p>
-                    </div>
+              <FormControl isRequired>
+                <FormLabel>Email Address</FormLabel>
+                <Input name="email" placeholder="Enter your email" type="email" />
+              </FormControl>
 
-                    <div className="infoContact">
-                      <div className="unicode-icon">&#10137;</div>
-                      {/* <span style='font-size:100px;'>&#128197;</span> */}
-                      <p className="left">8:00am - 5:00pm</p>
-                    </div>
+              <FormControl isRequired>
+                <FormLabel>Message</FormLabel>
+                <Textarea name="message" placeholder="Type your message here" />
+              </FormControl>
 
-                    <div className="infoContact">
-                      <div className="unicode-icon">&#x2706;</div>
-                      <p className="left">+1 801-669-2282</p>
-                    </div>
-
-                </div>
-                
-              </div>
-                  <div className="emailContact">
-                      <div  className="titleContact" >
-                            <p className="center"><h3>Mail Us:</h3></p>
-                      </div>
-                      <div className="infoC">
-                        <div className="infoContact">
-                          <div className="unicode-icon">&#128386;</div>
-                          <p className="left email">sjenergydoula@gmail.com</p>
-                        </div>
-
-                      </div>
-                      
-                  </div>
-            </div>
-            <div className="appointmentContact center">
-                <p className="left textContact"><h4 className="bookAppContact">We would be happy to see you with us soon:</h4></p>
-                {/* <button className="btnContact">Book an appoinment</button> */}
-                <Link className="titleLink btnContact" to="/login">Book an appointment</Link>
-            </div>
-          </div>
-
-
-        </div>
-
+              <Button type="submit" colorScheme="teal" width="full">
+                Send Message
+              </Button>
+            </VStack>
+          </form>
+        </Box>
 
         
-      </div>
-    );
-  }
+        <Box
+          flex="1"
+          p={8}
+          bg="white"
+          borderWidth={1}
+          borderRadius="lg"
+          boxShadow="xl"
+          ml={{ base: 0, lg: 10 }}  
+          mt={{ base: 10, lg: 0 }}  
+          width={{ base: "100%", lg: "80%" }}
+        >
+          <Heading as="h2" size="lg" mb={6}>
+            Contact Information
+          </Heading>
+
+          <Text fontSize="lg" mb={4}>
+            Phone: +1 801-669-2282
+          </Text>
+          <Text fontSize="lg" mb={4}>
+            Email: sjenergydoula@gmail.com
+          </Text>
+          <Text fontSize="lg" mb={4}>
+            Address: 123 Business Street, City, Country
+          </Text>
+        </Box>
+      </Flex>
+
+      {/* Modal */}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Email Sent Successfully</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            Your message has been sent. We will get back to you shortly.
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default Contact;
